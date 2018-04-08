37f23cac4ee9dbc9b069abc92154592342509f65
Jack <33677306+JACKSHINE2@users.noreply.github.com>
2018-04-08 16:54:46 +0800

    UI

---
+++ UIStruct/UIEventTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIEventTrigger :EventTrigger {
+
+
+    public delegate void Handler();
+ 
+    //public static UIEventTrigger Get(GameObject go)
+    //{
+    //    UIEventTrigger trigger = go.GetComponent<UIEventTrigger>();
+    //    if (trigger == null)
+    //        trigger = go.AddComponent<UIEventTrigger>();
+    //    return trigger;
+    //}
+    /// <summary>
+    /// 当单击的时候
+    /// </summary>
+    public event Handler onClick;
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if (onClick != null)
+            onClick();
+    }
+    /// <summary>
+    /// 当拖拽的时候
+    /// </summary>
+    public event Handler onDrag;
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if (onDrag != null)
+            onDrag();
+    }
+    /// <summary>
+    /// 当开始拖拽的时候
+    /// </summary>
+    public event Handler onBeginDrag;
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        if (onBeginDrag != null)
+            onBeginDrag();
+    }
+    /// <summary>
+    /// 当选择的时候
+    /// </summary>
+    public event Handler onSelect;
+    public override void OnSelect(BaseEventData eventData)
+    {
+        if (onSelect != null)
+            onSelect();
+    }
+
+
+}
---
+++ UIStruct/UISence.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 管理所有的UI子物体
+/// </summary>
+public class UISence : MonoBehaviour {
+    /// <summary>
+    /// 管理所有组件容器
+    /// </summary>
+    public Dictionary<string, UIEventTrigger>
+        uiTransDic = new Dictionary<string, UIEventTrigger>();
+
+    public void Start()
+    {
+        Init();
+    }
+    /// <summary>
+    /// 初始化所有需要被管理的子控件
+    /// </summary>
+    public void Init()
+    {
+        int tChildCount = transform.childCount;
+        UIEventTrigger trigger;
+        for (int i = 0; i < tChildCount; i++)
+        {
+            trigger = transform.GetChild(i)
+                .GetComponent<UIEventTrigger>();
+            if (trigger != null)
+                uiTransDic.Add(trigger.gameObject.name,
+                    trigger);
+        }
+    }
+    /// <summary>
+    /// 获取子物体
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public UIEventTrigger GetChild(string name)
+    {
+        if (uiTransDic.ContainsKey(name))
+            return uiTransDic[name];
+        return null;
+    }
+    public T GetChild<T>(string name) where T : Component
+    {
+      UIEventTrigger go=  GetChild(name);
+
+        if (go == null) return null;
+        T t = go.GetComponent<T>();
+        if (t != null)
+            return t;
+        return null;
+        
+    }
+    /// <summary>
+    /// 获取子物体
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public Transform FindChild(string name)
+    {
+        return SceneHelper.FindChild(this.transform, name);
+    }
+    public T FindChild<T>(string name) where T : Component
+    {
+        return SceneHelper.FindChild<T>(this.transform, name);
+    }
+}
+public class SceneHelper
+{
+    public static Transform FindChild(Transform item,string name)
+    {
+        Transform t = null;
+        t = item.Find(name);
+        if (t != null) return t;
+        for (int i = 0; i < item.childCount; i++)
+        {
+            t = FindChild(item.GetChild(i),name);
+            if (t != null)
+                return t;
+        }
+        return null;
+    }
+
+
+    public static T FindChild<T>(Transform item, string name)
+        where T : Component
+    {
+        Transform t = null;
+        t = item.Find(name);
+        if (t != null) return t.GetComponent<T>();
+        for (int i = 0; i < item.childCount; i++)
+        {
+            t = FindChild(item.GetChild(i), name);
+            if (t != null)
+                return t.GetComponent<T>();
+        }
+        return null;
+    }
+
+}
